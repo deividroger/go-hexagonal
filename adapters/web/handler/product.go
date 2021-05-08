c53dbb75f0b9eb82f9df120d620bdbe5abbeb04c
Deivid Roger Oliveira Santos <deivid@deividroger.net>
2021-05-08 19:09:09 -0300

    finalizado enable/disable product

---
+++ adapters/web/handler/product.go
@@ -0,0 +1,149 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/deividroger/go-hexagonal/adapters/dto"
+	"github.com/deividroger/go-hexagonal/application"
+	"github.com/gorilla/mux"
+)
+
+func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.ProductServiceInterface) {
+	r.Handle("/products/{id}", n.With(
+		negroni.Wrap(getProduct(service)),
+	)).Methods("GET", "OPTIONS")
+
+	r.Handle("/products/", n.With(
+		negroni.Wrap(createProduct(service)),
+	)).Methods("POST", "OPTIONS")
+
+	r.Handle("/products/{id}/disable", n.With(
+		negroni.Wrap(disableProduct(service)),
+	)).Methods("PUT", "OPTIONS")
+
+	r.Handle("/products/{id}/enable", n.With(
+		negroni.Wrap(enableProduct(service)),
+	)).Methods("PUT", "OPTIONS")
+
+}
+
+func getProduct(service application.ProductServiceInterface) http.Handler {
+
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+		product, err := service.Get(id)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusNotFound)
+			return
+		}
+		err = json.NewEncoder(rw).Encode(product)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+	})
+}
+
+func createProduct(service application.ProductServiceInterface) http.Handler {
+
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "application/json")
+		var productDto dto.Product
+		err := json.NewDecoder(r.Body).Decode(&productDto)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write(jsonError(err.Error()))
+			return
+		}
+		product, err := service.Create(productDto.Name, productDto.Price)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write(jsonError(err.Error()))
+			return
+		}
+
+		err = json.NewEncoder(rw).Encode(product)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write(jsonError(err.Error()))
+			return
+		}
+
+	})
+}
+
+func enableProduct(service application.ProductServiceInterface) http.Handler {
+
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+		product, err := service.Get(id)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusNotFound)
+
+			return
+		}
+
+		result, err := service.Enable(product)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write(jsonError(err.Error()))
+			return
+		}
+
+		err = json.NewEncoder(rw).Encode(result)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write(jsonError(err.Error()))
+			return
+		}
+
+	})
+}
+
+func disableProduct(service application.ProductServiceInterface) http.Handler {
+
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+		product, err := service.Get(id)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusNotFound)
+
+			return
+		}
+
+		result, err := service.Disable(product)
+
+		if err != nil {
+			rw.Write([]byte(err.Error()))
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		err = json.NewEncoder(rw).Encode(result)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write(jsonError(err.Error()))
+			return
+		}
+
+	})
+}
---
+++ adapters/web/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/codegangsta/negroni"
+	"github.com/deividroger/go-hexagonal/adapters/web/handler"
+	"github.com/deividroger/go-hexagonal/application"
+	"github.com/gorilla/mux"
+)
+
+type WebServer struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebServer() *WebServer {
+	return &WebServer{}
+}
+
+func (w WebServer) Serve() {
+
+	r := mux.NewRouter()
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+
+	handler.MakeProductHandlers(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":9000",
+		Handler:      http.DefaultServeMux,
+		ErrorLog:     log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
