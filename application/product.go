aeb8fb12bc3790469ea3e27af1ce2d561efa1009
Deivid Roger Oliveira Santos <deivid@deividroger.net>
2021-05-07 23:36:23 -0300

    ajustes

--- application/product.go
+++ application/product.go
@@ -23,7 +23,7 @@ type ProductInterface interface {
 }
 
 type ProductServiceInterface interface {
-	Get(id string) (Product, error)
+	Get(id string) (ProductInterface, error)
 	Create(name string, price float64) (ProductInterface, error)
 	Enable(product ProductInterface) (ProductInterface, error)
 	Disable(product ProductInterface) (ProductInterface, error)
